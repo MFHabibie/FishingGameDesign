db83f9cf74d5ec63c0614390364eb1471c163ccc
Muhammad Fariz Habibie <mfhabibie1906@gmail.com>
2024-08-19 15:30:38 +0700

    Fix folder
    Fix random generator folder

--- Scripts/Utility/RandomGenerator.h
+++ Scripts/Utility/RandomGenerator.h
@@ -6,7 +6,12 @@
 class RandomGenerator
 {
 public:
-	static int RandomRange(int min, int max);
-	static std::list<int> RandomWithSum(int sum);
+	/// <summary>
+	/// Random number generator
+	/// </summary>
+	/// <param name="min">: Inclusive number</param>
+	/// <param name="max">: Inclusive number</param>
+	/// <returns>Random number between min and max</returns>
+	int RandomRange(int min, int max);
 };
 
